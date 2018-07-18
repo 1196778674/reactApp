1583f2c9575ed06d5bf1ac83723df9194ac13c3a
yw <1196778674@qq.com>
2018-07-18 18:01:03 +0800

    css

--- app/page/list/List.js
+++ app/page/list/List.js
@@ -1,16 +1,40 @@
 import React, {Component} from "react";
-import {StyleSheet, Image, Text} from "react-native";
+import {
+	StyleSheet, 
+	Image, 
+	TextInput,
+	View,
+	Text
+} from "react-native";
 
 export default class SelectedList extends Component {
 	constructor(props) {
 	  super(props);
 	
-	  this.state = {};
+	  this.state = {
+	  	text: ''
+	  };
 	}
 
 	render () {
 		return (
-			<Text>list</Text>
+			<View>
+				<TextInput  style={{ borderColor: '#000000', borderWidth: 1, }}
+		          style={{height: 40}}
+		          placeholder="Type here to translate!"
+		          onChangeText={(text) => this.setState({text})}
+		        />
+			</View>
 		)
 	}
 }
+
+const ListStyle = StyleSheet.create({
+	border_box: {
+		paddingLeft: 2,
+		borderWidth: 1,
+		// borderStyle: 'solid',
+		borderColor: 'red'
+	},
+
+})
