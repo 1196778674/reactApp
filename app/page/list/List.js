2ac1d854c97dba3c5f371683615ca48ec672b3a3
yw <1196778674@qq.com>
2018-07-19 17:14:33 +0800

    t

--- app/page/list/List.js
+++ app/page/list/List.js
@@ -4,37 +4,73 @@ import {
 	Image, 
 	TextInput,
 	View,
+	FlatList,
 	Text
 } from "react-native";
+
+import Banner from "react-native-banner";
 
 export default class SelectedList extends Component {
 	constructor(props) {
 	  super(props);
+
+	  this.banners = [
+            {
+                title: 'beauty 1',
+                image: '../../image/home_page_header_cover.jpg',
+            },
+            {
+                title: 'beauty 2',
+                image: '../../image/ic_tab_strip_icon_feed_selected.png',
+            },
+            {
+                title: 'The next banner has no title',
+                image: '../../image/ic_tab_strip_icon_category.png',
+            },
+            {
+                title: 'no title',
+                image: '../../image/home_page_header_cover.jpg',
+            },
+        ];
 	
 	  this.state = {
-	  	text: ''
-	  };
+            clickTitle: 'You can try clicking beauty',
+            defaultIndex: 0,
+        }
+		this.defaultIndex = 0;	
+
 	}
+
 
 	render () {
 		return (
-			<View>
-				<TextInput  style={{ borderColor: '#000000', borderWidth: 1, }}
-		          style={{height: 40}}
-		          placeholder="Type here to translate!"
-		          onChangeText={(text) => this.setState({text})}
-		        />
-			</View>
+			<View style={styles.container}>
+                <Banner
+                    banners={this.banners}
+                    defaultIndex={this.defaultIndex}
+                    onMomentumScrollEnd={this.onMomentumScrollEnd.bind(this)}
+                    intent={this.clickListener.bind(this)}
+                />
+                <Text>{this.state.clickTitle}</Text>
+            </View>
 		)
 	}
+
+	clickListener(index) {
+        this.setState({
+            clickTitle: this.banners[index].title ? `you click ${this.banners[index].title}` : 'this banner has no title',
+        })
+    }
+
+    onMomentumScrollEnd(event, state) {
+        console.log(`--->onMomentumScrollEnd page index:${state.index}, total:${state.total}`);
+        this.defaultIndex = state.index;
+    }
 }
 
-const ListStyle = StyleSheet.create({
-	border_box: {
-		paddingLeft: 2,
-		borderWidth: 1,
-		// borderStyle: 'solid',
-		borderColor: 'red'
-	},
-
-})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        // backgroundColor: '#F5FCFF',
+    },
+});
