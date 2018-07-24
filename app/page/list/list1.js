7af02834242bc5d60b0a9de640fb3ab64b160158
yw <1196778674@qq.com>
2018-07-24 17:03:30 +0800

    native-base

--- app/page/list/list1.js
+++ app/page/list/list1.js
@@ -1,9 +1,25 @@
 import React, {Component} from 'react';
 import {View, TextInput} from 'react-native';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text, Card, CardItem, CheckBox, ListItem, Accordion } from 'native-base';
+
+
+// async componentWillMount() {
+//   await Expo.Font.loadAsync({
+//     'Roboto': require('native-base/Fonts/Roboto.ttf'),
+//     'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+//   });
+// }
+
+
+const dataArray = [
+  { title: "First Element", content: "Lorem ipsum dolor sit amet" },
+  { title: "Second Element", content: "Lorem ipsum dolor sit amet" },
+  { title: "Third Element", content: "Lorem ipsum dolor sit amet" }
+];
 
 
 export default class SelectedList1 extends Component {
+
     constructor(props) {
       super(props);
     
@@ -15,19 +31,52 @@ export default class SelectedList1 extends Component {
     render () {
         return (
             <Container>
-                <Header>
-                    <Body>
-                        <Title>Header</Title>
-                    </Body>
-                </Header>
-
-                <Content>
+            <Header>
+                <Text>title</Text>
+            </Header>
+            <Content padder>
+              <Card>
+                <CardItem header bordered>
+                  <Text>NativeBase</Text>
+                </CardItem>
+                <CardItem bordered>
+                  <Body>
                     <Text>
-                        This is Content Section
+                      NativeBase is a free and open source framework that enable
+                      developers to build
+                      high-quality mobile apps using React Native iOS and Android
+                      apps
+                      with a fusion of ES6.
                     </Text>
-                </Content>
-                
-            </Container>
+                  </Body>
+                </CardItem>
+                <CardItem footer bordered>
+                  <Text>GeekyAnts</Text>
+                </CardItem>
+              </Card>
+            </Content>
+
+            <Content padder>
+              <Card>
+                <CardItem header button onPress={() => alert("This is Card Header")}>
+                  <Text>NativeBase</Text>
+                </CardItem>
+                <CardItem button onPress={() => alert("This is Card Body")}>
+                  <Body>
+                    <Text>
+                      Click on any carditem
+                    </Text>
+                  </Body>
+                </CardItem>
+                <CardItem footer button onPress={() => alert("This is Card Footer")}>
+                  <Text>GeekyAnts</Text>
+                </CardItem>
+              </Card>
+            </Content>
+
+            
+
+          </Container>
         )
     }
 
